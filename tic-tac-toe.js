59fc7a4f9c8f8c47de7c5c0fcf5e45b8db0c4c7d
Jevon Forrest <jevonforrest77@gmail.com>
2021-10-08 00:36:57 -0500

    update new game javascript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,28 @@
 window.onload = function() {
     var xArray = [[],[],[],[],[],[],[],[]];
     var oArray = [[],[],[],[],[],[],[],[]];
-    var counter = 0;
     var id = 0;
-    var x = true;
+    var x = true; //used to alternate X and O insertions
     var status = document.getElementById("status");
     var gameBoard = document.getElementById("board");
     var boardList = gameBoard.querySelectorAll("div");
+    var button = document.querySelector("button");
+
 
     boardList.forEach( element => {
         element.setAttribute("class", "square");
         element.setAttribute("id", id);
-        id++;
+        id++; //ensures each div has a unique ID
         element.addEventListener( "click", clicked =>{
+
+
             if (x) {
                 element.textContent = "X";
                 element.classList.add("X");
-                counter++;
-                var num = element.getAttribute("id");
 
+                var num = element.getAttribute("id"); //gets id number for selected div (string)
+
+                // adds the ID of the divs marked for X or O into the xArray elements
                 if (parseInt(num) == 0 || parseInt(num) == 1 || parseInt(num) == 2){
                     xArray[0].push(parseInt(num));
                     xArray[0].sort();
@@ -45,13 +49,15 @@ window.onload = function() {
                     xArray[7].sort();
                 }
 
-                x = false;
+                x = false; //allows O to be the next player
             } else if (!x) {
                 element.textContent = "O";
                 element.classList.add("O");
-                counter++;
-                var num = element.getAttribute("id")
 
+                var num = element.getAttribute("id"); //gets id number for selected div (string)
+
+                // adds the ID of the divs marked for X or O into the oArray elements
+                //sorted so it can be easily compared to the possible combinations of X and O to win
                 if (parseInt(num) == 0 || parseInt(num) == 1 || parseInt(num) == 2){
                     oArray[0].push(parseInt(num));
                     oArray[0].sort();
@@ -78,27 +84,33 @@ window.onload = function() {
                     oArray[7].sort();
                 }
 
-                x = true;
+                x = true; //allows X to be the next player
 
             }
+
+            //checks if X won
             xArray.forEach( element => {
                 if (element.length == 3) {
-                    console.log(element);
                     status.textContent = "Congratulations! X is the Winner!";
                     status.classList.add("you-won");
+                    oArray = [[],[],[],[],[],[],[],[]]; //prevents winner from changing after one was declared
                }
             } )
 
+            //checks if O won
             oArray.forEach( element => {
                 if (element.length == 3) {
-                    console.log(element);
                     status.textContent = "Congratulations! O is the Winner!";
                     status.classList.add("you-won");
+                    xArray = [[],[],[],[],[],[],[],[]]; //prevents winner from changing after one was declared
                }
             } )
 
-        })
+            //the first player's array to achieve any of the following combinations wins:
+            //[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 
+        })
+        //adds hover effect to boxes
         element.onmouseover = mouseOn => {
             element.classList.add("hover");
         }
@@ -106,6 +118,22 @@ window.onload = function() {
             element.classList.remove("hover");
         }
 
+    })
+
+    //resets all variables and classes to normal
+    button.addEventListener("click", restart => {
+        boardList.forEach( element => {
+            element.textContent = "";
+            element.classList.remove("X");
+            element.classList.remove("O");
+            xArray = [[],[],[],[],[],[],[],[]];
+            oArray = [[],[],[],[],[],[],[],[]];
+            id = 0;
+            x = true;
+            status.textContent = "Move your mouse over a square and click to play an X or an O.";
+            status.classList.remove("you-won");
+
+        })
     })
 
 }
